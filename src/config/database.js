88c6886d5c85e78c3b16b50d6309907d6be25619
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-04-19 14:11:55 -0400

    implementação/postgres/migrations

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '2211',
+  database: 'code',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
