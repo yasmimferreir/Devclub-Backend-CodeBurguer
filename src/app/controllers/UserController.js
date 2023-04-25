f98947499c4115ad433f3479de56af968c0e3922
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-04-25 15:22:59 -0400

    implementação/uuid/controllers

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,5 @@
+// class userController(){
+//  store(request, response){
+
+//    }
+// }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { v4 } from 'uuid';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello" });
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: '',
+    email: '',
+    password_hash: '',
+    admin: true,
+  });
+
+  return response.json(user);
 });
 
 export default routes;
