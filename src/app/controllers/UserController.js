e0b1bc2047afcc1b982d9b0dace1c35fa9abdaa0
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-05-02 15:09:04 -0400

    atualização/backend/controllers

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -25,6 +25,16 @@ class UserController {
 
     const { name, email, password_hash, admin } = request.body;
 
+    const userExists = await User.findOne({
+      where: { email },
+    });
+
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' });
+    }
+
+    console.log(userExists);
+
     const user = await User.create({
       id: v4(),
       name,
