97d2e4c9bb7cc0a1655fe2087d9200031e258fbf
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-05-12 15:17:05 -0400

    atualização/src/controllers/Jwt

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
-
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(request, response) {
@@ -31,7 +32,15 @@ class SessionController {
       userEmailOrPasswordIncorrect();
     }
 
-    return response.json({ id: user.id, email, name: user.name, admin: user.admin });
+    return response.json({
+      id: user.id,
+      email,
+      name: user.name,
+      admin: user.admin,
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
