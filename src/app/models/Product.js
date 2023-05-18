fb3dffbc0bc18e8281d075204c0ccf483653f515
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-05-18 11:02:40 -0400

    atualização/models/product

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,24 +1,27 @@
-import { Sequelize, Model } from "sequelize";
+import { Sequelize, Model } from 'sequelize';
 
 class Product extends Model {
-    static init(sequelize) {
-        super.init({
-            name: Sequelize.STRING,
-            price: Sequelize.INTEGER,
-            category: Sequelize.STRING,
-            path: Sequelize.STRING,
-            url: {
-                type: Sequelize.VIRTUAL,
-                get(){
-                    return `http://localhost:3000/product-file/${this.path}`
-                }
-            }
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        price: Sequelize.INTEGER,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3000/product-file/${this.path}`;
+          },
         },
-        {
-            sequelize
-        }
-        )
-    }
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  //static associate(models) { }
 }
 
-export default Product
+export default Product;
